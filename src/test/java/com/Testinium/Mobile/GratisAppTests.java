f8cb028ef46814c8266ad96e42b056d228d4ab8c
Furkan Kartal <Furkan.kartal@testinium.com>
2025-11-30 21:17:50 +0300

    deneme

--- src/test/java/com/Testinium/Mobile/GratisAppTests.java
+++ src/test/java/com/Testinium/Mobile/GratisAppTests.java
@@ -44,42 +44,5 @@ public class GratisAppTests extends HookImpl {
 
     }
 
-    @org.junit.jupiter.api.Test
-    public void AndroidBasicTestFail() throws InterruptedException {
-
-        step.waitBySecond(3);
-        appiumDriver.findElement(By.id("com.gratis.android:id/nav_graph_trademarks")).click();
-        step.waitBySecond(3);
-        appiumDriver.findElement(By.id("com.gratis.android:id/nav_graph_categories")).click();
-        step.waitBySecond(3);
-        appiumDriver.findElement(By.id("com.gratis.android:id/nav_graph_campaignnnnn")).click();
-        step.waitBySecond(3);
-        appiumDriver.findElement(By.id("com.gratis.android:id/nav_graph_trademarks")).click();
-        step.waitBySecond(3);
-        appiumDriver.findElement(By.id("com.gratis.android:id/nav_graph_categories")).click();
-        step.waitBySecond(3);
-        appiumDriver.findElement(By.id("com.gratis.android:id/nav_graph_campaign")).click();
-        step.waitBySecond(3);
-
-    }
-
-    @org.junit.jupiter.api.Test
-    public void IOSBasicTestFail() throws InterruptedException {
-
-        step.waitBySecond(3);
-        appiumDriver.findElement(By.xpath("//XCUIElementTypeButton[@name=\"Markalar\"]")).click();
-        step.waitBySecond(3);
-        appiumDriver.findElement(By.xpath("//XCUIElementTypeButton[@name=\"Kategoriler\"]")).click();
-        step.waitBySecond(3);
-        appiumDriver.findElement(By.xpath("//XCUIElementTypeButton[@name=\"Kampanyalarasdsadsad\"]")).click();
-        step.waitBySecond(3);
-        appiumDriver.findElement(By.xpath("//XCUIElementTypeButton[@name=\"Markalar\"]")).click();
-        step.waitBySecond(3);
-        appiumDriver.findElement(By.xpath("//XCUIElementTypeButton[@name=\"Kategoriler\"]")).click();
-        step.waitBySecond(3);
-        appiumDriver.findElement(By.xpath("//XCUIElementTypeButton[@name=\"Kampanyalar\"]")).click();
-        step.waitBySecond(3);
-
-    }
 
 }
