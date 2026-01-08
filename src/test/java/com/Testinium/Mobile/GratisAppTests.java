c6d00bd8a570dcc6e148c1b2730ad0874bc625a7
Furkan Kartal <Furkan.kartal@testinium.com>
2026-01-08 13:25:23 +0300

    deneme

--- src/test/java/com/Testinium/Mobile/GratisAppTests.java
+++ src/test/java/com/Testinium/Mobile/GratisAppTests.java
@@ -29,11 +29,15 @@ public class GratisAppTests extends HookImpl {
     public void AndroidBasicTestSuccess01() throws InterruptedException {
 
         step.waitBySecond(3);
+        System.out.println("AndroidBasicTestSuccess01 started");
         appiumDriver.findElement(By.id("com.gratis.android:id/nav_graph_trademarks")).click();
+        System.out.println("Clicked trademarks");
         step.waitBySecond(3);
         appiumDriver.findElement(By.id("com.gratis.android:id/nav_graph_categories")).click();
+        System.out.println("Clicked categories");
         step.waitBySecond(3);
         appiumDriver.findElement(By.id("com.gratis.android:id/nav_graph_campaignnnn")).click();
+        System.out.println("Clicked campaign");
         step.waitBySecond(3);
         appiumDriver.findElement(By.id("com.gratis.android:id/nav_graph_trademarks")).click();
         step.waitBySecond(3);
